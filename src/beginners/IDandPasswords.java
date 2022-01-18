bbcc209d3da46f736616853404ab76fe7b06da60
John <kgimase@gmail.com>
2022-01-18 15:01:52 +0300

    page deleted
    deleted unwanted print record page

--- src/beginners/IDandPasswords.java
+++ src/beginners/IDandPasswords.java
@@ -5,14 +5,8 @@ import java.util.HashMap;
 public class IDandPasswords {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HashMap<String, String>logininfor=new HashMap<String, String>();
-		
-		    }
-		
+		HashMap<String, String> logininfor = new HashMap<String, String>();
+
 	}
 
-
-		
-	
-
-
+}
